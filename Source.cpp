114c4542802544ec14f5c3ae014bd06caf69f03a
Tom Wimer <ipodtouch_guy@hotmail.com>
2021-03-21 16:27:24 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+/*
+Thomas Wimer
+CIS 1202.NR1 Hello World
+Langley
+03/18/2021
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "\nHello World" << endl << endl;
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
